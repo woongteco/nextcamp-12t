4ac43a01db202515afbc48236d743b1587164435
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-26 17:36:16 +0900

    [Style] Noto Sans 한글 교체

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Noto_Sans } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
-const notosans = Noto_Sans({ subsets: ["latin"] });
+const notosans = Noto_Sans_KR({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
