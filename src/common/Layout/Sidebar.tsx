3018a8c4c06b5333d82f9d8efdd70afc2e7b4856
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-28 14:46:46 +0900

    [Style] 사이드바 레이아웃 컴포넌트 추가

---
+++ src/common/Layout/Sidebar.tsx
@@ -0,0 +1,85 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import { TMenuItemProps } from "@/types/component/props";
+
+export function NavItem({ href, label, active = false }: TMenuItemProps) {
+  return (
+    <li>
+      <Link
+        href={href}
+        className={`w-full inline-block rounded-[20px] text-H4 px-6 py-[26px] ${
+          active
+            ? "text-main-600 bg-main-25 hover:bg-main-50"
+            : "text-label-alt bg-white hover:bg-label-alt/20"
+        }`}
+      >
+        {label}
+      </Link>
+    </li>
+  );
+}
+
+function SideNav({
+  menus,
+  action = null,
+}: {
+  menus: TMenuItemProps[];
+  action?: React.ReactNode | null;
+}) {
+  return (
+    <ul className="flex flex-col gap-1">
+      {menus.map((item) => (
+        <NavItem key={item.href} {...item} />
+      ))}
+      {action}
+    </ul>
+  );
+}
+
+function Container({ children }: { children: React.ReactNode }) {
+  return children;
+}
+
+/**
+ * @기능
+ *  1. flex-col xl:grid 반응형
+ *  2. Sidebar.Nav와 Sidebar.Container가 각 한번씩만 사용
+ * @디자인
+ *  1. [xl] 1:3 그리드의 Sidebar.Nav와 Sidebar.Container 기본 레이아웃
+ *  2. [~1279px] flex column 레이아웃 시 NavItem 드롭다운
+ *  3. Sidebar.Nav의 menus 반복, 현재 페이지표시
+ */
+function Sidebar({ children }: { children: ReactNode }) {
+  if (React.Children.count(children) !== 2) {
+    throw new Error(
+      "Sidebar 컴포넌트는 Sidebar.Nav와 Sidebar.Container 컴포넌트를 하나씩 가져야만합니다."
+    );
+  }
+
+  const childrenArr = React.Children.toArray(children);
+  const nav = childrenArr.filter((child) => child.type === SideNav);
+  const container = childrenArr.filter((child) => child.type === Container);
+
+  if (nav.length !== 1) {
+    throw new Error(
+      "Sidebar 컴포넌트 내부에는 Sidebar.Nav가 반드시 하나 필요합니다"
+    );
+  }
+  if (container.length !== 1) {
+    throw new Error(
+      "Sidebar 컴포넌트 내부에는 Sidebar.Container가 반드시 하나 필요합니다"
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-[1fr_3fr] gap-[30px]">
+      {nav[0]}
+      {container[0]}
+    </div>
+  );
+}
+
+export default Object.assign(Sidebar, {
+  Nav: SideNav,
+  Container: Container,
+});
