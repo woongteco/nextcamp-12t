50ce802cabc26e88e2f023291d6e1d33e9535d3f
areumYang <yangareum1818@nate.com>
2024-06-29 00:51:09 +0900

    [Chore] : footer logo svg -> png로 변경

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -2,7 +2,6 @@ export { default as Logo } from "./logo/Logo.svg";
 export { default as CreateStudyIcon } from "./CreateStudyIcon.svg";
 export { default as AlarmIcon } from "./AlarmIcon.svg";
 
-export { default as FLogo } from "./logo/F-Logo.png";
 export { default as InstagramIcon } from "./sns/InstagramIcon.svg";
 export { default as LinkedinIcon } from "./sns/LinkedinIcon.svg";
 export { default as XIcon } from "./sns/XcomIcon.svg";
