bd0ee76fe8c5cd2b844456a64b6b9b897332b415
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-29 23:04:38 +0900

    [Chore] rename

---
+++ src/common/Organisms/StudyCardItem.tsx
@@ -0,0 +1,7 @@
+/**
+ * @props type: "default" (| "link" | "wide" | "large")
+ * - default는 가장 기본적으로 사용되는 스터디 카드 UI 디자인입니다.
+ * - link type은 커뮤니티 글 상세 페이지에서만 사용되기 때문에 common 컴포넌트 내에서는 직접 구현되지 않습니다.
+ * - wide type과 large type은 스터디 탐색 페이지에서만 사용되기 때문에
+ * common 컴포넌트 내에서는 직접 구현되지 않습니다.
+ */
