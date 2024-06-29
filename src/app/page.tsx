bf820191a789b578ac0dd557c9f620c88fd6a63f
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-29 22:59:19 +0900

    [Chore] children: ReactNode props 타입 통일

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-import Container from "@/common/Layout/Container";
 import MainLayout from "./(route)/layout";
 
-type Props = { children: React.ReactNode };
-
-export default function Home({ children }: Props) {
-  return <MainLayout children={children} />;
+export default function Home() {
+  return (
+    <MainLayout>
+      <p className="text-H3">
+        케밋에서 케미 터지는 스터디 장과 멤버들을 만나, <br />
+        놀라운 성장을 경험하세요!
+      </p>
+    </MainLayout>
+  );
 }
--- src/types/component/props.ts
+++ src/types/component/props.ts
@@ -1,9 +1,20 @@
 import { ComponentProps, ReactNode } from "react";
 
+export type TProps = {
+  children: ReactNode;
+};
+export type TIconButtonProps = ComponentProps<"button"> & {
+  variation: "icon";
+};
 export type TButtonProps = ComponentProps<"button"> & {
-  variation?: "solid" | "outline" | "icon";
-  color?: string;
+  variation: "solid" | "outline" | "text";
+  colors?: {
+    bg: string;
+    text?: string;
+  };
   fullWidth?: boolean;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
 };
 export type TLabelProps = ComponentProps<"label"> & {
   required: boolean;
@@ -12,6 +23,7 @@ export type TCaptionProps = ComponentProps<"p" | "span"> & {
   inline: boolean;
 };
 export type TMenuItemProps = {
+  key: string;
   href: string;
   label: string;
   active?: boolean;
