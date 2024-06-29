9cafc826cea2e2111c1eae8db67951ab4410269a
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-06-29 23:06:28 +0900

    [Chore] Container 컴포넌트 export 추가

--- src/common/Layout/index.tsx
+++ src/common/Layout/index.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Container from "./Container";
 import Sidebar from "./Sidebar";
-export { Header, Footer, Sidebar };
+export { Header, Footer, Container, Sidebar };
