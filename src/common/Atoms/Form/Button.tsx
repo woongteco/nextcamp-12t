d3a898bdf6ee5f58b1c7cf378ddede2b0008805d
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-01 00:41:00 +0900

    [Refactor] LinkButton 컴포넌트 추가, atomStyles 공유로 Button 컴포넌트 리팩토링

--- src/common/Atoms/Form/Button.tsx
+++ src/common/Atoms/Form/Button.tsx
@@ -1,10 +1,13 @@
 import { TButtonProps, TIconButtonProps } from "@/types/component/props";
+import { getStyles } from "../atomStyle";
+import clsx from "clsx";
 
 export default function Button(props: TIconButtonProps | TButtonProps) {
   switch (props.variation) {
     case "icon": {
       const {
         variation,
+        className,
         size = 40,
         colors = { bg: "bg-main-600" },
         children,
@@ -12,7 +15,12 @@ export default function Button(props: TIconButtonProps | TButtonProps) {
       const square = `w-[${size}px] h-[${size}px]`;
       return (
         <button
-          className={`flex items-center justify-center rounded-lg ${square} ${colors.bg}/0 hover:${colors.bg}/15`}
+          className={clsx(
+            className,
+            "flex items-center justify-center rounded-lg",
+            `${colors.bg}/0 hover:${colors.bg}/15`,
+            square
+          )}
         >
           {children}
         </button>
@@ -21,23 +29,20 @@ export default function Button(props: TIconButtonProps | TButtonProps) {
     default: {
       const {
         variation,
+        className,
         colors = { bg: "bg-main-600", text: "text-white" },
         startIcon = null,
         endIcon = null,
+        fullWidth = false,
         children,
         ...restProps
       } = props;
-      const border = "border-" + colors.bg.split("bg-").at(0);
-      const styles = {
-        solid: `${colors.bg} ${colors.text} hover:${colors.bg}/75 hover:saturate-150`,
-        outline: `border ${border} ${colors.bg}/0 ${colors.text} hover:${colors.bg}/15`,
-        text: `${colors.text} hover:${colors.bg}/15`,
-      };
       return (
         <button
-          className={`flex flex-row gap-2 px-5 py-3 font-bold rounded-[10px] ${
-            styles[variation]
-          } ${restProps.fullWidth ? "w-full" : "w-fit"}`}
+          className={clsx(
+            "flex flex-row items-center justify-center gap-2 px-5 py-3 font-bold text-nowrap text-ellipsis overflow-hidden rounded-ten",
+            [getStyles(colors, variation), "w-full" && fullWidth, className]
+          )}
           {...restProps}
         >
           {startIcon || null}
