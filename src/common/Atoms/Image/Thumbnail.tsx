c5a1f069a9f9126aee2666fb3bd2bd34ac6bcec1
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-01 10:39:29 +0900

    [Chore] props type 수정

--- src/common/Atoms/Image/Thumbnail.tsx
+++ src/common/Atoms/Image/Thumbnail.tsx
@@ -4,15 +4,14 @@ import { TClassnameProps } from "@/types/component/props";
 import clsx from "clsx";
 
 /**
- *
+ * TODO: 썸네일 사용 시 이용 타입에 따라 추가 개발 필요
  * @param type card-thumbnail(스터디 카드), detail-thumbnail(스터디 상세 페이지), linked-thumbnail(커뮤니티 관련 스터디 링크)
  */
 export default function Thumbnail({
   altText = "썸네일",
   className,
-}: {
+}: TClassnameProps & {
   altText?: string;
-  className?: TClassnameProps;
 }) {
   return (
     <Image
