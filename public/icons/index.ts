10f65c8f82bbdb7ab4154436d11cc0cf9184536e
oweaj <qwe1520156@naver.com>
2024-07-06 17:00:55 +0900

    [Chore] 누락 login svg 추가

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -4,6 +4,12 @@ export { default as CreateStudyIcon } from "./CreateStudyIcon.svg";
 export { default as AlarmIcon } from "./AlarmIcon.svg";
 export { default as heartIcon } from "./heart.svg";
 
+export { default as Kakao } from "./login/Kakao.svg";
+export { default as Google } from "./login/Google.svg";
+export { default as SocialLogin } from "./login/SocialLogin.svg";
+export { default as PasswordCheck } from "./login/PasswordCheck.svg";
+export { default as PasswordHide } from "./login/PasswordHide.svg";
+
 export { default as InstagramIcon } from "./sns/InstagramIcon.svg";
 export { default as LinkedinIcon } from "./sns/LinkedinIcon.svg";
 export { default as XIcon } from "./sns/XcomIcon.svg";
