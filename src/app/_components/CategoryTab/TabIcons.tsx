1f12bc334c2eb6a18df534cb4bd99d9a3d663cb8
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-07 16:28:28 +0900

    [Chore] move category icon

---
+++ src/app/_components/CategoryTab/TabIcons.tsx
@@ -0,0 +1,21 @@
+import {
+  BellRingIcon,
+  BuildingIcon,
+  BulbIcon,
+  FileEditIcon,
+  KeyboardIcon,
+  MonitorPlayIcon,
+  NotebookIcon,
+  PuzzleIcon,
+} from "@/common/Atoms/Image/Icon";
+
+export const ICONS = [
+  NotebookIcon,
+  KeyboardIcon,
+  BulbIcon,
+  FileEditIcon,
+  BuildingIcon,
+  BellRingIcon,
+  MonitorPlayIcon,
+  PuzzleIcon,
+];
