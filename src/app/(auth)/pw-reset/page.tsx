254a6096a4c97f0956788b3088b7525be6257031
oweaj <qwe1520156@naver.com>
2024-07-07 15:00:55 +0900

    [Chore] 경로 수정 및 더블쿼터 설정

--- src/app/(auth)/pw-reset/page.tsx
+++ src/app/(auth)/pw-reset/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import { Logo } from '@public/icons';
-import { UserEmail } from '../login/_components/UserInput';
+import Image from "next/image";
+import { Logo } from "@public/icons";
+import { UserEmail } from "../(login)/login/_components/UserInput";
 
 export default function PWresetPage() {
   return (
@@ -8,7 +8,8 @@ export default function PWresetPage() {
       <Image src={Logo} alt="logo" />
       <h1 className="text-xl font-semibold">비밀번호 찾기</h1>
       <p className="text-center text-sm text-gray-700">
-        가입한 이메일을 입력하면 <br /> 이메일로 비밀번호 변경 링크가 전송됩니다.
+        가입한 이메일을 입력하면 <br /> 이메일로 비밀번호 변경 링크가
+        전송됩니다.
       </p>
       <UserEmail />
       <button className="w-full rounded-md py-2 text-white bg-main-600">
