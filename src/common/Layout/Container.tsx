9ee76e71a4555f881513aaa4b98f6e4981d11ada
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-15 16:02:55 +0900

    [Refactor] 공통 컴포넌트 업데이트

--- src/common/Layout/Container.tsx
+++ src/common/Layout/Container.tsx
@@ -2,7 +2,7 @@ import { TProps } from "@/types/component/props";
 
 export default function Container({ children }: TProps) {
   return (
-    <div className="xl:container xl:w-[1200px] xl:mx-auto mx-auto px-4">
+    <div className="xl:container xl:w-[1200px] xl:mx-auto px-4 xl:px-0">
       {children}
     </div>
   );
---
+++ src/common/Molecules/Pagination.tsx
@@ -0,0 +1,79 @@
+import { TButtonProps, TIconButtonProps } from "@/types/component/props";
+import Button from "../Atoms/Form/Button";
+import { PaginationChevronIcon } from "../Atoms/Image/Icon";
+import { Dispatch, SetStateAction } from "react";
+
+type TPages = {
+  length: number;
+  current: number;
+  setCurrent: Dispatch<SetStateAction<number>>;
+};
+export default function Pagination(props: TPages) {
+  const { length, current, setCurrent } = props;
+  const pageNums = Array(length)
+    .fill(1)
+    .map((num, idx) => num + idx);
+  const isStart = current === 1;
+  const isEnd = current === length;
+  return (
+    <div className="mt-20 flex gap-8 items-center justify-center">
+      <PageButton
+        variation="icon"
+        disabled={isStart}
+        onClick={() => {
+          if (!isStart) {
+            setCurrent(current - 1);
+          }
+        }}
+      >
+        <PaginationChevronIcon direction="prev" />
+      </PageButton>
+      <div className="flex items-center justify-center">
+        {pageNums.map((page) => (
+          <PageButton
+            active={current === page}
+            onClick={() => setCurrent(page)}
+          >
+            {page}
+          </PageButton>
+        ))}
+      </div>
+      <PageButton
+        variation="icon"
+        disabled={isEnd}
+        onClick={() => {
+          if (!isEnd) {
+            setCurrent(current + 1);
+          }
+        }}
+      >
+        <PaginationChevronIcon direction="next" />
+      </PageButton>
+    </div>
+  );
+}
+
+function PageButton(
+  props: Partial<TIconButtonProps | TButtonProps> & { active?: boolean }
+) {
+  const {
+    active = false,
+    variation = "text",
+    onClick,
+    children,
+    ...restProps
+  } = props;
+  return (
+    <Button
+      variation={"text"}
+      onClick={onClick}
+      colors={{
+        bg: "bg-label-assist",
+        text: active ? "text-label-strong" : "text-label-assist",
+      }}
+      {...restProps}
+    >
+      {children}
+    </Button>
+  );
+}
