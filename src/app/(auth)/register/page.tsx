b75bd7161c34ec58d940903083e099633ff94cec
oweaj <qwe1520156@naver.com>
2024-07-16 00:28:05 +0900

    [Chore] 컴포넌트 분리로 인한 props 삭제

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -9,27 +9,12 @@ const passwordRoles = [
   "연속 3자 이상 동일한 문자/숫자 제외",
 ];
 
-const serviceCheck = [
-  {
-    id: "service",
-    service: "서비스 이용약관 동의 (필수)",
-  },
-  {
-    id: "privacy",
-    service: "개인정보 수집 및 이용 동의 (필수)",
-  },
-  {
-    id: "marketing",
-    service: "마케팅 수신 동의 (선택)",
-  },
-];
-
 export default function Register() {
   return (
     <AuthWrap style="p-6">
       <Image src={Logo} alt="logo" />
       <h1 className="text-xl font-semibold">이메일로 회원가입</h1>
-      <RegisterForm roles={passwordRoles} services={serviceCheck} />
+      <RegisterForm roles={passwordRoles} />
     </AuthWrap>
   );
 }
