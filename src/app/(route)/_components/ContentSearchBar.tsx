33af855c4a4fddeafd68fbe0b2c0cb32277c757e
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-15 15:59:26 +0900

    [Refactor] 검색어 입력창 컴포넌트 분리

---
+++ src/app/(route)/_components/ContentSearchBar.tsx
@@ -0,0 +1,28 @@
+export default function ContentSearchBar() {
+  return (
+    <div className="relative">
+      <input
+        type="search"
+        placeholder="검색어를 입력하세요"
+        className="w-[380px] py-2 pr-9 pl-6 border border-line-normal rounded-lg outline-offset-1 focus-visible:outline-1 focus-visible:outline-main-600 placeholder:text-label-assist placeholder:text-label-nomral"
+      />
+      <span className="absolute right-2 top-2">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M15 15L21 21M10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10C17 13.866 13.866 17 10 17Z"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
+    </div>
+  );
+}
