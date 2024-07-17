f5cea906fe856a620211727f79fc5c7e50a57232
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-17 17:54:52 +0900

    [Fix] add middleware config: unstable_allowDynamic: https://nextjs.org/docs/messages/edge-dynamic-code-evaluation

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,9 @@ import { getSession } from "./auth";
 
 export const config = {
   runtime: "experimental-edge", // for Edge API Routes only
+  unstable_allowDynamic: [
+    "/node_modules/function-bind/**", // use a glob to allow anything in the function-bind 3rd party module
+  ],
   matcher: ["/my/:path*", "/post/write"],
 };
 
