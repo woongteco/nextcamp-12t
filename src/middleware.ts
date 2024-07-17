61fc9b51c49c47c644398778172eae770ddae620
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-17 18:06:06 +0900

    [Fix] try to

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,7 +18,7 @@ export async function middleware(request: NextRequest) {
 
   // 세션정보 확인하고 있는경우 통과 없는경우에는 로그인 모달창 떠야하는데 경로가 아닌점 고민
   // 클라이언트 구분 하기
-  if (session) {
-    return NextResponse.next();
-  }
+  // if (session) {
+  return NextResponse.next();
+  // }
 }
