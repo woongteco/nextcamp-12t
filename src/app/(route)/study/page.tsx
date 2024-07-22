1b9b6ebf7504baeb5d0c42b85db2ac310b58e2ee
oweaj <qwe1520156@naver.com>
2024-07-22 23:04:16 +0900

    [Chore] session 가져오기 테스트

--- src/app/(route)/study/page.tsx
+++ src/app/(route)/study/page.tsx
@@ -3,7 +3,6 @@ import StudyCardList from "@/common/Templates/CardList";
 import LargeStudyList from "./_components/LargeStudyList";
 import WideStudyList from "./_components/WideStudyList";
 
-import { getUser } from "@/dummies/user";
 import { getStudiesData } from "@/dummies/studies";
 import { GOALS } from "@/constants/categories/study_goal";
 import { ONOFF } from "@/constants/categories/study_type";
@@ -15,6 +14,7 @@ import {
   categoryIconsName,
   onOffIconsName,
 } from "@/app/_components/CategoryTab/TabIcons";
+import { getSession } from "@/auth";
 
 export type TQuery = {
   job_c?: string;
@@ -23,12 +23,12 @@ export type TQuery = {
   sort?: string;
 };
 
-export default function StudyComponent({
+export default async function StudyComponent({
   searchParams,
 }: {
   searchParams: TQuery;
 }) {
-  const user = getUser();
+  const session = await getSession();
   const studyCard = getStudiesData();
 
   return (
@@ -56,7 +56,7 @@ export default function StudyComponent({
         <div className="flex gap-[6.25rem] flex-col">
           <div>
             <SectionTitle size="md" className="pb-6">
-              {user.name}님이 찾던 케미 좋은 프로 스터디
+              {session?.user.name}님이 찾던 케미 좋은 프로 스터디
             </SectionTitle>
             <div className="flex flex-col gap-6">
               <div>
