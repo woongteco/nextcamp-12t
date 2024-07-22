c4ed9e81a517d40299786e8b711a5030fc25a709
oweaj <qwe1520156@naver.com>
2024-07-23 00:24:49 +0900

    [Fix] 컴포넌트 에러 수정

--- src/app/(auth)/pw-reset/page.tsx
+++ src/app/(auth)/pw-reset/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Logo } from "@public/icons";
-import { UserEmail } from "../_components/UserInput";
 import AuthWrap from "../_components/AuthWrap";
+import { Input } from "../_components/UserInput";
 
 export default function PWresetPage() {
   return (
@@ -12,7 +12,12 @@ export default function PWresetPage() {
         가입한 이메일을 입력하면 <br /> 이메일로 비밀번호 변경 링크가
         전송됩니다.
       </p>
-      <UserEmail />
+      <Input
+        id="email"
+        type="email"
+        title="이메일"
+        placeholder="example@chemeet.com"
+      />
       <button className="w-full rounded-md py-2 text-white bg-main-600">
         비밀번호 변경 링크 전송하기
       </button>
