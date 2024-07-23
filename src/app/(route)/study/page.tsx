2f3b982b31074c09926c7cb91f68bc668c959d39
areumYang <yangareum1818@nate.com>
2024-07-23 12:02:17 +0900

    + 충돌내용추가

--- src/app/(route)/study/page.tsx
+++ src/app/(route)/study/page.tsx
@@ -29,7 +29,7 @@ export default async function StudyComponent({
   searchParams: TQuery;
 }) {
   const session = await getSession();
-  const studyCard = getStudiesData();
+  const studyCards = getStudiesData();
 
   return (
     <>
@@ -54,46 +54,45 @@ export default async function StudyComponent({
         </div>
       </div>
 
-        <div className="flex gap-[6.25rem] flex-col">
-          <div>
-            <SectionTitle size="md" className="pb-6">
-              {session?.user.name}님이 찾던 케미 좋은 프로 스터디
-            </SectionTitle>
-            <div className="flex flex-col gap-6">
-              <div>
-                <SectionTitle size="sm" className="pb-3">
-                  디자인 프로 스터디
-                </SectionTitle>
-                <WideStudyList studyCards={studyCards} count={2} />
-              </div>
-              <div>
-                <SectionTitle size="sm" className="pb-3">
-                  취업 / 포트폴리오 프로 스터디
-                </SectionTitle>
-                <WideStudyList studyCards={studyCards} count={2} />
-              </div>
+      <div className="flex gap-[6.25rem] flex-col">
+        <div>
+          <SectionTitle size="md" className="pb-6">
+            {session?.user.name}님이 찾던 케미 좋은 프로 스터디
+          </SectionTitle>
+          <div className="flex flex-col gap-6">
+            <div>
+              <SectionTitle size="sm" className="pb-3">
+                디자인 프로 스터디
+              </SectionTitle>
+              <WideStudyList studyCards={studyCards} count={2} />
+            </div>
+            <div>
+              <SectionTitle size="sm" className="pb-3">
+                취업 / 포트폴리오 프로 스터디
+              </SectionTitle>
+              <WideStudyList studyCards={studyCards} count={2} />
             </div>
           </div>
-          <div>
-            <SectionTitle size="md" className="pb-6">
-              오늘의 프로 스터디를 통해 빠른 성장을 경험해 보세요!
-            </SectionTitle>
-            <StudyCardList studyCards={studyCards} count={4} />
-          </div>
-          <div>
-            <SectionTitle size="md" className="pb-6">
-              프로없이, 우리끼리도 스터디해요
-            </SectionTitle>
-            <StudyCardList studyCards={studyCards} count={4} />
-          </div>
-          <div>
-            <SectionTitle size="md" className="pb-6">
-              케밋에서 인기있는 프로의 스터디
-            </SectionTitle>
-            <LargeStudyList studyCards={studyCards} count={3} />
-          </div>
+        </div>
+        <div>
+          <SectionTitle size="md" className="pb-6">
+            오늘의 프로 스터디를 통해 빠른 성장을 경험해 보세요!
+          </SectionTitle>
+          <StudyCardList studyCards={studyCards} count={4} />
+        </div>
+        <div>
+          <SectionTitle size="md" className="pb-6">
+            프로없이, 우리끼리도 스터디해요
+          </SectionTitle>
+          <StudyCardList studyCards={studyCards} count={4} />
+        </div>
+        <div>
+          <SectionTitle size="md" className="pb-6">
+            케밋에서 인기있는 프로의 스터디
+          </SectionTitle>
+          <LargeStudyList studyCards={studyCards} count={3} />
         </div>
       </div>
-    </div>
+    </>
   );
 }
