e9a5764eda169e82859970063da89962b01bdbaf
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-25 03:29:27 +0900

    [Feat] 댓글 업데이트

--- src/common/Organisms/Comment/CommentItem.tsx
+++ src/common/Organisms/Comment/CommentItem.tsx
@@ -1,41 +1,66 @@
 "use client";
 import Profile from "@/common/Molecules/Profile";
-import { TComment } from "@/common/Templates/CommentArea";
+import { CommentSchema } from "@/types/model/Comment";
+import { useState } from "react";
+import CommentInput from "./CommentInput";
 
 function CommentBodyLayout({
   comment,
+  canEdit,
   canReply = true,
 }: {
-  comment: Omit<TComment, "reply">;
+  comment: Omit<CommentSchema, "reply">;
+  canEdit: boolean;
   canReply?: boolean;
 }) {
+  const [writeReply, setWrite] = useState<boolean>(false);
+  const createdAt = new Intl.DateTimeFormat("ko-KR", {
+    dateStyle: "medium",
+    timeStyle: "medium",
+  });
   return (
     <>
       <p className="text-body-400 text-label-normal">{comment.content}</p>
       <p className="text-label-400 text-label-dimmed flex flex-row flex-nowrap gap-6">
-        <span>{comment.createdAt}</span>
-        {canReply && <button>답글쓰기</button>}
-        {/* 아래 수정, 삭제 버튼은 작성자=로그인한 사용자인 경우, */}
-        <button>수정</button>
-        <button>삭제</button>
+        <span>{createdAt.format(Date.parse(comment.createdAt))}</span>
+        {canReply && <button onClick={() => setWrite(true)}>답글쓰기</button>}
+        {canEdit && (
+          <>
+            <button>수정</button>
+            <button>삭제</button>
+          </>
+        )}
       </p>
+      {writeReply && (
+        <CommentInput init={true} onCancel={() => setWrite(false)} />
+      )}
     </>
   );
 }
 
-export default function CommentItem({ comment }: { comment: TComment }) {
+export default function CommentItem({
+  comment,
+  canEdit,
+}: {
+  comment: CommentSchema;
+  canEdit: boolean;
+}) {
   return (
     <div className="flex flex-col gap-5 border-t py-6 border-t-line-normal">
       <Profile size="default" user={comment.writer!} />
       <div className="flex flex-col gap-5 pl-14">
-        <CommentBodyLayout comment={comment} />
+        <CommentBodyLayout comment={comment} canEdit={canEdit} />
         {comment?.reply &&
           comment.reply.length > 0 &&
           comment.reply.map((reply) => (
             <div className="flex flex-col gap-5 pt-6" key={reply.commentId}>
               <Profile size="default" user={reply.writer} />
               <div className="flex flex-col gap-5 pl-14">
-                <CommentBodyLayout comment={reply} canReply={false} />
+                <CommentBodyLayout
+                  comment={reply}
+                  canReply={false}
+                  canEdit={canEdit}
+                />
               </div>
             </div>
           ))}
