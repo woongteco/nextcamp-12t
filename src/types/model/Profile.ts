719cb5fec842a59740b6e95a8480e35cebc829fb
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-30 16:21:21 +0900

    [Chore] 프로필 스키마 타입 추가

--- src/types/model/Profile.ts
+++ src/types/model/Profile.ts
@@ -2,8 +2,11 @@ import { UserSchema } from "./User";
 
 export type ProfileSchema = {
   _id: object;
-  userId: UserSchema;
   position_tag: string;
   introduce: string;
   my_category: string[];
+};
+
+export type ProfileFullDataSchema = ProfileSchema & {
+  userId: UserSchema;
 };
