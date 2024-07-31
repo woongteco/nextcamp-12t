2cb005ca6ec2999b93e71068e160fbc0c67a06a7
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-31 14:32:18 +0900

    [Chore] 파일 위치 변경

--- src/app/(route)/my/profile/_components/EditProfile/FormEditProfile.tsx
+++ src/app/(route)/my/profile/_components/EditProfile/FormEditProfile.tsx
@@ -1,37 +1,102 @@
+"use client";
+
+import { ChangeEvent, FormEvent } from "react";
+import Input from "@/common/Molecules/Form/Input";
+import ProfileInputArea from "../ProfileInputArea";
 import Button from "@/common/Atoms/Form/Button";
-import ProfileFormsAndPreview from "./ProfileFormsAndPreview";
-import { profileAction } from "@/lib/actions/profileAction";
+import { CATEGORIES } from "@/constants/categories/job_category";
+import { useRouter } from "next/navigation";
+import { profileAction, updateProfile } from "@/lib/actions/profileAction";
 import { TProfileData } from "@/types/model/Profile";
+import { Session } from "next-auth";
+import handleAlert from "@/common/Molecules/handleAlert";
 
-type FormEditProfileProps = {
-  userId: string;
+export default function FormEditProfile({
+  session,
+  profile,
+}: {
+  session: Session | null;
   profile: TProfileData;
-};
+}) {
+  const router = useRouter();
 
-export default function FormEditProfile(props: FormEditProfileProps) {
-  const { userId, profile } = props;
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-  async function save(formData: FormData) {
-    "use server";
-    // e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const id = session?.user.id;
 
-    // const userId = profile?.userId._id as string;
-    // const formData = new FormData(e.currentTarget);
+    if (!id) {
+      handleAlert("error", "사용자 ID가 없습니다.");
+      return;
+    }
 
     try {
-      await profileAction(userId, formData);
-      // handleAlert("success", "프로필 정보가 저장되었습니다.");
-    } catch (error: any) {
-      // handleAlert("error", error.message);
-      console.error("error", error.message);
+      const result = profile
+        ? await updateProfile(id, formData)
+        : await profileAction(id, formData);
+
+      if (result.state) {
+        handleAlert("success", result.message);
+
+        if (!profile) {
+          router.replace("/my/profile");
+        }
+      } else {
+        handleAlert("error", result.message);
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   return (
-    <form action={save} className="flex flex-col gap-8">
-      <ProfileFormsAndPreview defaultValue={profile} />
+    <form onSubmit={handleSubmit} className="flex flex-col gap-8">
+      <ProfileInputArea label="포지션 태그">
+        <Input.Text
+          name="positionTag"
+          placeholder="이름 앞에 추가될 포지션 태그를 추가하세요"
+          defaultValue={profile.position_tag}
+        />
+      </ProfileInputArea>
+      <ProfileInputArea label="내 소개">
+        <Input.Textarea
+          name="introduce"
+          placeholder="나를 소개할 말을 추가하세요"
+          defaultValue={profile.introduce}
+        />
+      </ProfileInputArea>
+      {session?.account.provider === "credentials" && (
+        <ProfileInputArea label="이메일">
+          <Input.Email
+            name="email"
+            defaultValue={session?.user.email || ""}
+            placeholder="이메일 주소를 입력하세요"
+          />
+          <div className="flex gap-4 items-center">
+            <Button
+              variation="outline"
+              colors={{ bg: "bg-main-600", text: "text-main-600" }}
+            >
+              이메일 인증
+            </Button>
+            <span className="text-label-400 text-main-600">
+              *변경 후 재인증이 필요합니다.
+            </span>
+          </div>
+        </ProfileInputArea>
+      )}
+      <ProfileInputArea label="관심 카테고리">
+        <Input.Select
+          name="interest"
+          placeholder="관심 카테고리를 추가하세요"
+          isMulti
+          options={CATEGORIES}
+          defaultValue={profile.my_category}
+        />
+      </ProfileInputArea>
       <Button type="submit" variation="solid" className="self-end">
-        프로필 저장
+        변경 내용 저장
       </Button>
     </form>
   );
--- src/app/(route)/my/profile/_components/ProfileForms.tsx
+++ src/app/(route)/my/profile/_components/ProfileForms.tsx
@@ -1,4 +1,4 @@
-import FormEditProfile from "./FormEditProfile";
+import FormEditProfile from "./EditProfile/FormEditProfile";
 import SectionTitle from "@/common/Atoms/Text/SectionTitle";
 import FormUpdatePassword from "./UpdatePassword/FormUpdatePassword";
 import FormUpdatePhoneNumber from "./UpdatePhoneNumber/FormUpdatePhoneNumber";
@@ -6,49 +6,50 @@ import DeleteAccountConfirm from "./DeleteAccountConfirm";
 import { getProfile } from "@/lib/actions/profileAction";
 import { getSession } from "@/auth";
 import FormEditProfileImageWithPreview from "./EditProfileImage/FormEditProfileImageWithPreview";
+import NotFound from "@/app/not-found";
 
-export default async function ProfileForms({ userId }: { userId: string }) {
+export default async function ProfileForms() {
   const session = await getSession();
-  const sessionProvider = session?.account.provider;
+
+  if (session === null) {
+    return <NotFound />;
+  }
+
+  const userId = session.user.id;
+  const sessionProvider = session.account.provider;
   const userProfile = await getProfile(userId);
   let profile = userProfile.data;
   let clientProfile = JSON.parse(JSON.stringify(profile));
 
+  console.log("id", session?.user.id);
+
   console.log("get 프로필 데이터" + JSON.stringify(profile));
 
   return (
-    <>
-      <div className="grid xl:grid-cols-[5fr_4fr] xl:items-start gap-gutter-xl">
-        <div className="flex flex-col gap-8">
-          <p className="text-H2 text-label-dimmed">
-            {session?.user.name as string}
-          </p>
-          <FormEditProfileImageWithPreview
-            id={session?.user.id as string}
-            initProfileUrl={profile.userId.id}
-          />
-          <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-          <FormEditProfile session={session} profile={clientProfile} />
-          {sessionProvider === "credentials" && (
-            <>
-              <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-              <SectionTitle size="md">비밀번호 수정</SectionTitle>
-              <FormUpdatePassword />
-            </>
-          )}
-          <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-          {sessionProvider === "credentials" && (
-            <>
-              <SectionTitle size="md" className="mb-2">
-                연락처 수정
-              </SectionTitle>
-              <FormUpdatePhoneNumber defaultValue={profile.userId.phone} />
-              <div className="w-full h-[1px] border-t border-t-line-normal"></div>
-            </>
-          )}
-          <DeleteAccountConfirm />
-        </div>
+    <div className="w-full lg:w-[calc(100vw-2rem-240px-30px)] xl:w-[calc(1200px-280px-30px)] grid xl:grid-cols-[5fr_4fr] xl:items-start gap-gutter-xl">
+      <div className="flex flex-col gap-8">
+        <p className="text-H2 text-label-dimmed">{session.user.name}</p>
+        <FormEditProfileImageWithPreview
+          id={session.user.id}
+          initProfileUrl={profile.userId.id}
+        />
+        <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+        <FormEditProfile session={session} profile={clientProfile} />
+        {sessionProvider === "credentials" && (
+          <>
+            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+            <SectionTitle size="md">비밀번호 수정</SectionTitle>
+            <FormUpdatePassword />
+            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+            <SectionTitle size="md" className="mb-2">
+              연락처 수정
+            </SectionTitle>
+            <FormUpdatePhoneNumber defaultValue={profile.userId.phone} />
+            <div className="w-full h-[1px] border-t border-t-line-normal"></div>
+            <DeleteAccountConfirm email={session.user.email as string} />
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
 }
