770bc49cd663aa3800926653dbed65d43a5fc08d
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-31 16:59:16 +0900

    [Feat] 계정 삭제 확인 모달 추가

--- src/app/(route)/my/profile/_components/DeleteAccountConfirm.tsx
+++ src/app/(route)/my/profile/_components/DeleteAccountConfirm.tsx
@@ -6,9 +6,29 @@ import Label from "@/common/Atoms/Form/Label";
 import Input from "@/common/Molecules/Form/Input";
 import handleAlert from "@/common/Molecules/handleAlert";
 import { unregisterAction } from "@/lib/actions/authAction";
+import useModal from "@/hooks/useModal";
 
 export default function DeleteAccountConfirm({ email }: { email: string }) {
   const [checked, setChecked] = useState<boolean>(false);
+  const { Modal, open } = useModal({
+    children: (
+      <div className="flex flex-col gap-6">
+        <p className="text-H3 text-label-normal">정말로 데이터를 삭제할까요?</p>
+        <div className="flex items-center justify-center gap-4">
+          <Button
+            variation="solid"
+            colors={{ bg: "bg-status-danger", text: "text-white" }}
+            className="border-status-danger"
+            onClick={unregister}
+          >
+            계정 삭제
+          </Button>
+        </div>
+      </div>
+    ),
+    key: "confirm-unregister",
+  });
+
   async function unregister() {
     try {
       const result = await unregisterAction(email);
@@ -46,10 +66,11 @@ export default function DeleteAccountConfirm({ email }: { email: string }) {
         colors={{ bg: "bg-status-danger", text: "text-status-danger" }}
         className="border-status-danger"
         disabled={!checked}
-        onClick={unregister}
+        onClick={open}
       >
         회원 탈퇴
       </Button>
+      {Modal}
     </div>
   );
 }
