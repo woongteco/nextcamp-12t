17c36ee8c23807e7af9c0a4a608e6ffdeaa3e076
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-07-31 14:35:29 +0900

    [Fix] document is not definded 오류 방지로 LoadingContainer 조건 추가

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,16 +1,25 @@
-import { useId, useRef, useState } from "react";
+"use client";
+import { Suspense, useEffect, useId, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
 import ModalPortal from "@/common/Molecules/ModalPortal/ModalPortal";
 import useDialogEscape from "./useDialogEscape";
 import { TPopoverHookParams } from "./usePopover";
+import LoadingContainer from "@/common/Layout/LoadingContainer";
 
 export default function useModal(props: TPopoverHookParams) {
   const { onClose, children, defaultValue = false, key = undefined } = props;
   const modalRef = useRef<HTMLDialogElement>(null);
   const uid = useId();
   const modalKey = key || uid;
+  const [dovEnv, setDocEnv] = useState(false);
   const [show, setShow] = useState(defaultValue);
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setDocEnv(true);
+    }
+  }, []);
 
   useDialogEscape({
     show,
@@ -31,15 +40,19 @@ export default function useModal(props: TPopoverHookParams) {
     }
   }
 
-  const element = show
-    ? createPortal(
+  const element = show ? (
+    dovEnv ? (
+      createPortal(
         <ModalPortal ref={modalRef} onClose={close}>
           {children}
         </ModalPortal>,
         document.body,
         modalKey
       )
-    : null;
+    ) : (
+      <LoadingContainer className="w-full h-full" />
+    )
+  ) : null;
 
   return {
     Modal: element,
