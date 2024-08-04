951d348ed6c19403ab8f1539bd1c231f4a9afdc3
oweaj <qwe1520156@naver.com>
2024-08-04 23:34:45 +0900

    [Chore] 미들웨어 경로 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
-// import { getSession } from "./auth";
+import { NextResponse } from "next/server";
+import { getSession } from "./auth";
 
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-};
-export async function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
-/* export const config = {
-  matcher: ["/my/:path*", "/post/write"],
-};
-
-export async function middleware(request: NextRequest) {
+export async function middleware() {
   const session = await getSession();
 
-  // 세션정보 확인하고 있는경우 통과 없는경우에는 로그인 모달창 떠야하는데 경로가 아닌점 고민
-  // 클라이언트 구분 하기
-  if (session) {
-    return NextResponse.next();
+  if (!session) {
+    return NextResponse.redirect("http://localhost:3000/login");
   }
-} */
+}
+
+export const config = {
+  matcher: ["/my/:path*", "/post/write", "/study/create"],
+};
