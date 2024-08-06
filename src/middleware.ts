25d45131ce007bbc7bb6cde6f96d2f0ff503c209
oweaj <qwe1520156@naver.com>
2024-08-06 11:34:40 +0900

    [Chore] 미들웨어 경로 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { getSession } from "./auth";
 
 export async function middleware() {
   const session = await getSession();
+  // 미들웨어 빌드 에러 session if 조건문 알아보기
 
   if (!session) {
     return NextResponse.redirect("http://localhost:3000/login");
