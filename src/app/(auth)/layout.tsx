cedd2721a7c83549653b5a082398f6d7cd8e1c8b
oweaj <qwe1520156@naver.com>
2024-08-12 00:31:24 +0900

    [Chore] session provier 누락 제거

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,7 @@
-import { SessionProvider } from "next-auth/react";
-
 export default async function LoginModalLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <>{children}</>;
 }
