3b4ded0132d59b1beee7d0f4f4681e9846b148e3
oweaj <qwe1520156@naver.com>
2024-08-12 00:30:49 +0900

    [Chore] user profile 스키마 수정

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -5,14 +5,6 @@ const subLabelValue = new mongoose.Schema({
   value: { type: String, required: true },
 });
 
-// @deprecated 프로필
-const profile = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  position_tag: { type: String, default: "" },
-  introduce: { type: String, default: "" },
-  my_category: { type: [subLabelValue], default: [] },
-});
-
 // 유저
 const user = new mongoose.Schema(
   {
@@ -112,8 +104,6 @@ const study = new mongoose.Schema({
 export const User = mongoose.models?.User || mongoose.model("User", user);
 export const Mypage =
   mongoose.models?.Mypage || mongoose.model("Mypage", mypage);
-export const Profile =
-  mongoose.models?.Profile || mongoose.model("Profile", profile);
 export const Post = mongoose.models?.Post || mongoose.model("Post", post);
 export const Study = mongoose.models?.Study || mongoose.model("Study", study);
 export const Comment =
