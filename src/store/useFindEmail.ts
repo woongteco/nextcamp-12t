7779aff72c30c6cec993f2a5622f95ebe6a1136a
oweaj <qwe1520156@naver.com>
2024-08-14 11:50:56 +0900

    [Feat] Find Email 전역상태관리

--- src/store/useFindEmail.ts
+++ src/store/useFindEmail.ts
@@ -1,13 +1,36 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type TFindEmail = {
   userEmail: string | null;
   setUserEmail: (email: string) => void;
+  clearUserEmail: () => void;
 };
 
-const useFindEmail = create<TFindEmail>((set) => ({
-  userEmail: null,
-  setUserEmail: (email: string) => set({ userEmail: email }),
-}));
+const useFindEmail = create(
+  persist<TFindEmail>(
+    (set) => ({
+      userEmail: null,
+      setUserEmail: (email: string) => {
+        const checkTime = Date.now() + 10 * 60 * 1000;
+        localStorage.setItem("storage-time", checkTime.toString()),
+          set({ userEmail: email });
+      },
+      clearUserEmail: () => {
+        set({ userEmail: null });
+        localStorage.removeItem("storage-time");
+      },
+    }),
+    {
+      name: "find-email",
+      onRehydrateStorage: () => (state) => {
+        const storageTime = localStorage.getItem("storage-time");
+        if (state && storageTime && Date.now() > parseInt(storageTime)) {
+          state.clearUserEmail();
+        }
+      },
+    }
+  )
+);
 
 export default useFindEmail;
