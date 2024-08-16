619223ce280b8f6633d5e58f8e6c551f3d9cd1da
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-08-16 12:35:26 +0900

    [Feat] getStudy() 테스트

--- src/app/_components/RecommendProStudies.tsx
+++ src/app/_components/RecommendProStudies.tsx
@@ -1,6 +1,11 @@
 import StudyCardList from "@/common/Templates/CardList";
-import { filterStudies, getAllStudies } from "@/lib/actions/studyAction";
+import {
+  filterStudies,
+  getAllStudies,
+  getStudy,
+} from "@/lib/actions/studyAction";
 import { getUserData } from "@/lib/actions/userAction";
+import { StudyDataFull } from "@/types/model/StudyCard";
 
 async function getFavorStudies(userId: string) {
   // 1. profile에서 관심 카테고리 가져오기
@@ -9,7 +14,7 @@ async function getFavorStudies(userId: string) {
   const profile = await getUserData(userId);
 
   if (profile.state === false) {
-    const result = await getAllStudies();
+    const result = await getStudy();
     return result;
   }
 
@@ -24,13 +29,13 @@ export default async function RecommendProStudies({
   userId: string;
 }) {
   const result =
-    userId === "" ? await getAllStudies() : await getFavorStudies(userId);
+    userId === "" ? await getStudy() : await getFavorStudies(userId);
 
   if (result.state === false) {
     throw new Error("관심 카테고리의 스터디 정보 가져오기 실패");
   }
 
-  const recommend = result.data;
+  const recommend: StudyDataFull[] = result.data;
 
   return <StudyCardList studyCards={recommend} count={8} />;
 }
