9af49ec235714594f8b3fd4597f2799c66edcdcc
oweaj <qwe1520156@naver.com>
2024-08-19 17:22:59 +0900

    [Chore] 콘솔 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
   const cookieSession = request.cookies.get("authjs.session-token");
 
-  console.log("미들웨어" + JSON.stringify(cookieSession));
-
   if (!cookieSession) {
     const loginUrl = new URL("/login", request.url);
 
