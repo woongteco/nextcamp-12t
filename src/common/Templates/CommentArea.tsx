60f1902b28d55bfdba6dc7360e760a12d5447e4b
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-08-20 22:54:41 +0900

    [Fix] 댓글/답글 수정 시 데이터 업데이트 딜레이 제거를 위해 상태값 사용

--- src/common/Templates/CommentArea.tsx
+++ src/common/Templates/CommentArea.tsx
@@ -30,7 +30,6 @@ export default async function CommentArea(props: TCommentArea) {
   if (userResult.state === true && commentResult.state === true) {
     user = userResult.data;
     comments = commentResult.data ?? [];
-    console.log("comments data", JSON.stringify(commentResult.data, null, 2));
   }
 
   return (
