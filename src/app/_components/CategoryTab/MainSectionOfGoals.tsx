1f2203fa0f2f13adb4ee5b00fdc9f6b1d669dc10
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-08-26 19:57:39 +0900

    [Fix] TypeError 수정 테스트 중

--- src/app/_components/CategoryTab/MainSectionOfGoals.tsx
+++ src/app/_components/CategoryTab/MainSectionOfGoals.tsx
@@ -1,29 +1,71 @@
 "use client";
+import { MouseEvent, useEffect, useState } from "react";
+import { GOALS } from "@/constants/categories/study_goal";
+import { categoryIconsName, CategoryTabIcon } from "./TabIcons";
 import { StudyDataFull } from "@/types/model/StudyCard";
 import Skeleton from "@/common/Atoms/Skeleton";
 import StudyCardList from "@/common/Templates/CardList";
-import mainTabSelectedStore from "@/store/mainTabStore";
-import { useEffect, useState } from "react";
+import { TabButton } from "./TabButton";
+
+const GOALS_TAB = GOALS.map((goal, index) => ({
+  ...goal,
+  iconName: categoryIconsName[index],
+}));
 
 export default function MainSectionOfGoals({
   data,
 }: {
   data: StudyDataFull[];
 }) {
-  const { mainTab } = mainTabSelectedStore();
-  const initStudies = data.filter(
-    (s) => s.studyInfo.targetCategory === mainTab
+  const initSelected: string = GOALS[0].value;
+  const initStudies: StudyDataFull[] = data.filter(
+    (s) => s.studyInfo.targetCategory === initSelected
   );
+  const [selected, setSelected] = useState<string>(initSelected);
   const [studies, setStudies] = useState<StudyDataFull[]>(initStudies);
-  useEffect(() => {
-    // console.log({ studies });
-    setStudies(() =>
-      data.filter((s) => s.studyInfo.targetCategory === mainTab)
-    );
-  }, [mainTab]);
+  // function changeSelecteTab(
+  //   e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
+  //   value: string
+  // ) {
+  //   e.preventDefault();
+  //   console.log(":selected:", value);
+  //   setSelected(value);
+  //   const newSelected: StudyDataFull[] = data.filter(
+  //     (s) => s.studyInfo.targetCategory === value
+  //   );
+  //   setStudies(newSelected);
+  // }
 
   return (
     <>
+      <div className="flex flex-wrap xl:flex-row gap-4 w-fit mx-auto mb-11">
+        {GOALS_TAB.map(({ label, value, iconName }) => {
+          const active = selected === value;
+          return (
+            <TabButton
+              key={value}
+              label={label}
+              active={active}
+              onClick={(
+                e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+              ) => {
+                e.preventDefault();
+                console.log(":selected:", value);
+                setSelected(value);
+                const newSelected: StudyDataFull[] = data.filter(
+                  (s) => s.studyInfo.targetCategory === value
+                );
+                setStudies(newSelected);
+              }}
+            >
+              <CategoryTabIcon
+                name={iconName}
+                strokeColor={active ? "#FFFFFF" : undefined}
+              />
+            </TabButton>
+          );
+        })}
+      </div>
       {studies.length > 0 ? (
         <StudyCardList studyCards={studies} count={8} />
       ) : (
