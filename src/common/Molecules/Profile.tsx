ba74d9acaf4d4b23f25b5a1a20665430180a5805
oweaj <qwe1520156@naver.com>
2024-08-28 13:38:24 +0900

    [Chore] Console 제거

--- src/common/Molecules/Profile.tsx
+++ src/common/Molecules/Profile.tsx
@@ -30,7 +30,6 @@ export default function Profile({
   };
 
   const src = user.profile_img || "/images/profile/DummyProfileImg.jpg";
-  console.log("profile user", user.name);
 
   return (
     <div className={clsx("flex flex-row flex-nowrap items-center", gap[size])}>
