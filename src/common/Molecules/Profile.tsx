bfd72d80ec37dadc276b3f2367374ccf1b38652c
YI Na-yeon <2n.ayeon.yi@gmail.com>
2024-08-29 12:50:40 +0900

    [Chore]

--- src/common/Molecules/Profile.tsx
+++ src/common/Molecules/Profile.tsx
@@ -30,7 +30,7 @@ export default function Profile({
   };
 
   const src = user.profile_img || "/images/profile/DummyProfileImg.jpg";
-  console.log("profile user", user.name);
+  // console.log("profile user", user.name);
 
   return (
     <div className={clsx("flex flex-row flex-nowrap items-center", gap[size])}>
