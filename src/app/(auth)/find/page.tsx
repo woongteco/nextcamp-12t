ecf893826d8dfaf4fb03ff898d96987bf8e85e5a
oweaj <qwe1520156@naver.com>
2024-09-23 23:23:15 +0900

    [Chore] 컴포넌트 중복 제거

--- src/app/(auth)/find/page.tsx
+++ src/app/(auth)/find/page.tsx
@@ -1,10 +1,9 @@
-import AuthWrap from "../_components/AuthWrap";
 import FindAuthWrap from "../_components/FindAuthWrap";
 
 export default function FindPage() {
   return (
-    <AuthWrap>
+    <>
       <FindAuthWrap title="이메일" />
-    </AuthWrap>
+    </>
   );
 }
